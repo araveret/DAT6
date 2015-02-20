1e3665770dd46684b8b5c4ccc5011cbbe6f96fa4
Josiah Davis <josiah.j.davis@gmail.com>
2015-02-20 08:08:44 -0500

    Added plotting

--- code/01_pandas_class.py
+++ code/01_pandas_class.py
@@ -22,6 +22,7 @@ Reading, Summarizing data
 
 import pandas as pd  # This line imports  (already installed) python package
 import numpy as np
+import matplotlib.pylab as plt
 
 # Running this next line of code assumes that your console working directory is set up correctly 
 # To set up your working directory
@@ -243,6 +244,53 @@ drinks.continent.value_counts()
 drinks[drinks.continent.isnull()].country
 
 '''
+Plotting
+'''
+
+# bar plot of number of countries in each continent
+drinks.continent.value_counts().plot(kind='bar', title='Countries per Continent')
+plt.xlabel('Continent')
+plt.ylabel('Count')
+plt.show()                                  # show plot window (if it doesn't automatically appear)
+plt.savefig('countries_per_continent.png')  # save plot to file
+
+# bar plot of average number of beer servings (per adult per year) by continent
+drinks.groupby('continent').beer_servings.mean().plot(kind='bar')
+plt.ylabel('Average Number of Beer Servings Per Year')
+
+# histogram of beer servings (shows the distribution of a numeric column)
+drinks.beer_servings.hist(bins=20)
+plt.xlabel('Beer Servings')
+plt.ylabel('Frequency')
+
+# density plot of beer servings (smooth version of a histogram)
+drinks.beer_servings.plot(kind='density', xlim=(0,500))
+plt.xlabel('Beer Servings')
+
+# grouped histogram of beer servings (shows the distribution for each group)
+drinks.beer_servings.hist(by=drinks.continent)
+drinks.beer_servings.hist(by=drinks.continent, sharex=True)
+drinks.beer_servings.hist(by=drinks.continent, sharex=True, sharey=True)
+drinks.beer_servings.hist(by=drinks.continent, layout=(1, 5))   # change layout (new in pandas 0.15.0)
+
+# boxplot of beer servings by continent (shows five-number summary and outliers)
+drinks.boxplot(column='beer_servings', by='continent')
+
+# scatterplot of beer servings versus wine servings
+drinks.plot(kind='scatter', x='beer_servings', y='wine_servings', alpha=0.3)
+
+# same scatterplot, except point color varies by 'spirit_servings'
+# note: must use 'c=drinks.spirit_servings' prior to pandas 0.15.0
+drinks.plot(kind='scatter', x='beer_servings', y='wine_servings', c=drinks.spirit_servings, colormap='Blues')
+
+# same scatterplot, except all European countries are colored red
+colors = np.where(drinks.continent=='EU', 'r', 'b')
+drinks.plot(x='beer_servings', y='wine_servings', kind='scatter', c=colors)
+
+# scatterplot matrix of all numerical columns
+pd.scatter_matrix(drinks)
+
+'''
 Indexing
 '''
 
@@ -431,7 +479,7 @@ ufo.replace('PYRAMID', 'TRIANGLE')       # replace values throughout a DataFrame
 
 
 '''
-Plotting
+Fun Stuff
 '''
 
 # Plot the number of sightings over time
